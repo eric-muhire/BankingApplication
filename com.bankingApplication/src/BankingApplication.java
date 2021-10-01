279a9f1791007890aafbb95310ac6103b7c3af76
eric-muhire <e.muhire24@gmail.com>
2021-10-01 17:53:28 +0200

    Have added application class

---
+++ com.bankingApplication/src/BankingApplication.java
@@ -0,0 +1,12 @@
+public class BankingApplication{
+
+    public static void main(String[] args) {
+
+    void showMenu(){
+        char option='\0';
+        Scanner scanner = new Scanner(System.in);
+
+        S.o
+
+    }
+        }
